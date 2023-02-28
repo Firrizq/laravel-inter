e2abb4077b961bb3d974101bc3404352914b566e
Firrizq <firrizq@gmail.com>
2023-02-28 10:59:58 +0700

    edit dit and nasabah tabungan

--- tests/Feature/DependencyInjectionTest.php
+++ tests/Feature/DependencyInjectionTest.php
@@ -2,22 +2,22 @@
 
 namespace Tests\Feature;
 
+use App\Data\Foo;
+use App\Data\Bar;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
-use App\Data\Bar;
-use App\Data\Foo;
 
 use function PHPUnit\Framework\assertEquals;
 
-class DependencyInjectTest extends TestCase
+class DependencyInjectionTest extends TestCase
 {
     /**
      * A basic feature test example.
      */
     public function TestDependencyInjection(){
         $foo = new foo();
-        $bar = new bar($foo);
+        $bar = new Bar($foo);
 
         assertEquals('fooand bar', $bar -> bar());
     }
